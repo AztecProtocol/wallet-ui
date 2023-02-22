ad1441f44e99faf0a162b6e04abd8539a3aa983d
sirasistant <sirasistant@gmail.com>
2023-02-22 14:18:14 +0000

    feat(ui): added app logo and name

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { ToastsProvider } from '../utils/toastsContext';
 import style from './app.module.scss';
+import logo from './assets/wallet-logo.svg';
 
 interface AppCardProps {
   children: ReactNode;
@@ -8,8 +9,14 @@ interface AppCardProps {
 
 export default function AppCard({ children }: AppCardProps) {
   return (
-    <div className={style.app}>
-      <ToastsProvider>{children}</ToastsProvider>
+    <div>
+      <div className={style.appHeader}>
+        <img className={style.appLogo} src={logo} alt="Wallet logo" />
+        <div className={style.appName}>Wallet.Aztec</div>
+      </div>
+      <div className={style.app}>
+        <ToastsProvider>{children}</ToastsProvider>
+      </div>
     </div>
   );
 }
